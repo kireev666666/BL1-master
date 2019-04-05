4beed22dbbe117e2bfacb58bc8dc3ea8cf4310e3
kireev_6666666@outlook.com <kireev_6666666@outlook.com>
2019-04-05 13:23:56 +0300

    ex1

--- Basic_Lesson_1/Program.cs
+++ Basic_Lesson_1/Program.cs
@@ -13,31 +13,55 @@ namespace Basic_Lesson_1
             Console.WriteLine("Здравсвуйте, меня зовут Василиса. Предоставьте список документов");
 
             var documents = new  string [1];
-            const int adultAge = 18;
+            const bool Photo = true;
 
             var document = Console.ReadLine();
             documents[0] = document;
 
-            Console.WriteLine("How old are you?");
-            var age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Do you have a photo?");
+            bool Photo1 = bool.Parse(Console.ReadLine());
 
-            if (age >= adultAge)
+            if (Photo1 == Photo)
             {
                 Console.WriteLine("Ok. Go to pay");
             }
             else
             {
-                Console.WriteLine("Do you have parent permission?");
-                bool hasPermision = bool.Parse(Console.ReadLine());
+                Console.WriteLine("Go ot photo room");
 
-                if (hasPermision == true)
+                Console.WriteLine("Do you have a photo?");
+                int Photo2 = Int32.Parse(Console.ReadLine());
+
+                if (Photo2 <= 3)
                 {
                     Console.WriteLine("Ok. Go to pay");
                 }
                 else
                 {
-                    Console.WriteLine("Fail. Go to parents");
-                }
+                    Console.WriteLine("Go to photo room");
+
+                    Console.WriteLine("Do you use photo in passport?");
+                    bool Photo3 = bool.Parse(Console.ReadLine());
+
+                    if (Photo3 == Photo)
+                    {
+                        Console.WriteLine("Go to photo room");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ok. Go to pay");
+
+                        Console.WriteLine("Do you paste photo in passport?");
+                        bool Photo4 = bool.Parse(Console.ReadLine());
+
+                        if (Photo4 == Photo)
+                        {
+                            Console.WriteLine("Ok. Go to pay");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Go to photo room");
+                        }
 
             }
 
